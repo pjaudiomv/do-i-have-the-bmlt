9755376dcfcc82afa6b4dfdd0474408e1365b127
jbraswell <jbraswell@localhost.localdomain>
2018-03-27 18:11:13 -0400

    Refactor BMLTResult... I'm learning

--- src/BMLTResult.js
+++ src/BMLTResult.js
@@ -1,44 +1,59 @@
 import React, { Component } from 'react';
 import { Button, Segment, Header,  Divider } from 'semantic-ui-react';
-import InfoLinks from './InfoLinks.js';
 
 class BMLTResult extends Component {
   render() {
     let miles = Math.round(Math.round(this.props.numMiles));
     let milesText = miles === 1 ? `${miles} mile` : `${miles} miles`;
 
-    if (this.props.hasBMLT) {
-      return (
-        <div>
-          <Header as="h3">Yes!</Header>
-          <p>The nearest meeting in our database is {milesText} from you, so we think your local service body probably is using the BMLT. If you don't think this is the case, follow the links below to learn more about how to get started!</p>
-          <Header as="h3">How do I learn more about the BMLT?</Header>
-          <InfoLinks/>
-          <Divider hidden/>
-          <SearchAgainButton onClick={this.props.searchAgainHandler}/>
-          <Divider/>
-          <Segment size="mini" secondary basic>Root server: {this.props.rootServerURL}</Segment>
-        </div>
-      );
-    } else {
-      return (
-        <div>
-          <Header as="h3">No ☹</Header>
-          <p>The nearest meeting in our database is {milesText} from you, so your local service body probably is not using the BMLT.</p>
-          <Header as="h3">I want to use the BMLT. How do I get started?</Header>
-          <InfoLinks/>
-          <Divider hidden/>
-          <SearchAgainButton onClick={this.props.searchAgainHandler}/>
-        </div>
-      );
-    }
-  }
-}
+    let getAnswer = () => {
+      if (this.props.hasBMLT) {
+        return 'Yes!';
+      }
+      return 'No ☹';
+    };
 
-class SearchAgainButton extends Component {
-  render() {
-    return <Button fluid onClick={this.props.onClick}>Search for Another Location</Button>;
-  };
+    let getContent = () => {
+      if (this.props.hasBMLT) {
+        return `The nearest meeting in our database is ${milesText} from you, so we think your local service body probably is using the BMLT. If you don't think this is the case, follow the links below to learn more about how to get started!`
+      }
+      return `The nearest meeting in our database is ${milesText} from you, so your local service body probably is not using the BMLT.`;
+    };
+
+    let getLearnMoreText = () => {
+      if (this.props.hasBMLT) {
+        return 'How do I learn more about the BMLT?';
+      }
+      return 'I want to use the BMLT. How do I get started?';
+    };
+
+    let getRootServerComponent = () => {
+      if (this.props.hasBMLT) {
+        return (
+          <div>
+            <Divider/>
+            <Segment size="mini" secondary basic>Root server: {this.props.rootServerURL}</Segment>
+          </div>
+        )
+      }
+      return <div/>;
+    };
+
+    return (
+      <div>
+        <Header as="h3">{getAnswer()}</Header>
+        <p>{getContent()}</p>
+        <Header as="h3">{getLearnMoreText()}</Header>
+        <ul>
+          <li>Visit the <a href="https://bmlt.magshare.net/" target="_blank" rel="noopener noreferrer">website</a>.</li>
+          <li>Join our <a href="https://www.facebook.com/groups/149214049107349/" target="_blank" rel="noopener noreferrer">Facebook Group</a>.</li>
+        </ul>
+        <Divider hidden/>
+        <Button fluid onClick={this.props.searchAgainHandler}>Search for Another Location</Button>
+        {getRootServerComponent()}
+      </div>
+    );
+  }
 }
 
 export default BMLTResult;
