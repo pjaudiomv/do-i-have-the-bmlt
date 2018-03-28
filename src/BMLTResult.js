9fb94368d9b0bf5423a456db295d96277124b1a7
jbraswell <jbraswell@localhost.localdomain>
2018-03-27 22:07:13 -0400

    Add missing semicolon

--- src/BMLTResult.js
+++ src/BMLTResult.js
@@ -15,7 +15,7 @@ class BMLTResult extends Component {
 
     let getContent = () => {
       if (this.props.hasBMLT) {
-        return `The nearest meeting in our database is ${milesText} from you, so we think your local service body probably is using the BMLT. If you don't think this is the case, follow the links below to learn more about how to get started!`
+        return `The nearest meeting in our database is ${milesText} from you, so we think your local service body probably is using the BMLT. If you don't think this is the case, follow the links below to learn more about how to get started!`;
       }
       return `The nearest meeting in our database is ${milesText} from you, so your local service body probably is not using the BMLT.`;
     };
